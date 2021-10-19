9837f06b6977a08e689e5872ed6d2aa8011e20fc
Harish <hariharan.g@tranxitgroup.com>
2021-10-19 15:16:42 +0530

    drop page dynamic content

--- src/components/new-drops/drop-card.js
+++ src/components/new-drops/drop-card.js
@@ -3,51 +3,36 @@ import Image from "react-bootstrap/Image";
 import NFTCounter from "../nft-counter";
 import "./style.scss";
 
-const DropCard = ({ img }) => {
+const DropCard = ({ img ,cardTitle, smallTitle, cardDesc, dropTitle, dropDescOne, dropDescTwo, auctionTitle, auctionTime, editionTitle, editionType, additional, additionalDesc }) => {
   return (
     <>
       <section className="dropCard-Section" id="drop_1">
         <div className="container">
           <div className="row">
             <div className="card_title">
-              <h2>Madhushala NFTs</h2>
+              <h2>{cardTitle}</h2>
               <p className="small-title mb-3">
-                Amalgamation Of Knowledge, Fulfilment of duties, and
-                Expectations
+                {smallTitle}
               </p>
               <p>
-                BeyondLife.club brings you the philosophical undertones of
-                Madhushala in Amitabh’s baritone as an NFT!. Now you can own a
-                recorded version of Madhushala, curated by Amitabh Bachchan
-                himself. By owning this one-of-a-kind NFT, you are owning
-                segments of Indian history, a livelihood full of extraordinary
-                scenarios, a guide for betterment, a betterment for a lifetime.
+                {cardDesc}
               </p>
             </div>
           </div>
           <div className="row drop_card">
             <div className="col-lg-6 border-right">
               <div className="drop-title">
-                <h4 className="mb-4">About Collection</h4>
+                <h4 className="mb-4">{dropTitle}</h4>
                 <p>
-                  Mr. Harivansh Rai Bachchan (1907–2003), father of Mr. Amitabh
-                  Bachchan, wrote Madhushala that depicts the wisdom of
-                  Madhu-temple of Mind (Madhushala), karma, imbibement of
-                  knowledge, fulfillment of duties, and expectations of an
-                  individual from the society into beautiful verses.
+                  {dropDescOne}
                 </p>
                 <p className="mb-4">
-                  Madhushala’s Rhyme, Rhythm, and Flavour is still fresh in
-                  Amitabh’s mind. He believed and witnessed the miracles that
-                  the purest form of art brings in society. However, the love
-                  and the respect Mr.Bachchan holds in his heart that compelled
-                  him to recite, sing, and translate stories of Madhushala from
-                  time to time.
+                  {dropDescTwo}
                 </p>
               </div>
               <div className="auction-time">
-                <h5>Auction starting in</h5>
-                <NFTCounter time="2021-10-21T04:26:07.014Z" />
+                <h5>{auctionTitle}</h5>
+                <NFTCounter time={auctionTime} />
               </div>
               <div class="auction-main">
                 <div class="auction-one">
@@ -55,17 +40,14 @@ const DropCard = ({ img }) => {
                   <h1>TBA</h1>
                 </div>
                 <div class="auction-two">
-                  <p class="heading-S">Edition of</p>
-                  <h1>2/2 </h1>
+                  <p class="heading-S">{editionTitle}</p>
+                  <h1>{editionType} </h1>
                 </div>
               </div>
               <div className="additional-perks">
-                <h5>Addtional Perk</h5>
+                <h5>{additional}</h5>
                 <p>
-                  The highest bidders in the two categories, in addition to the
-                  NFTs, will also get to avail a meet-and-greet session with the
-                  Big B - A memory that any Amitabh will hold on to more dearly
-                  than the NFT itself!
+                  {additionalDesc}
                 </p>
               </div>
             </div>
