1016b38994f4abb8d5f906558b0eb144a83eae1c
vijayguardian <88785341+vijayguardian@users.noreply.github.com>
2021-10-20 22:22:10 +0530

    Drop page content changes

--- src/components/new-drops/drop-card.js
+++ src/components/new-drops/drop-card.js
@@ -31,53 +31,53 @@ const DropCard = ({
 
   return (
     <>
-      
-        <div className="container">
-          <div className="row">
-            <div className="card_title col-lg-6">
-              <h2>{cardTitle}</h2>
-              <p className="small-title mb-3">{smallTitle}</p>
-              <p>{cardDesc}</p>
+      <div className="container">
+        <div className="row">
+          <div className="card_title">
+            <h2>{cardTitle}</h2>
+            <p className="small-title mb-3">{smallTitle}</p>
+            <p>{cardDesc}</p>
+          </div>
+        </div>
+        <div className="row drop_card">
+          <div className="col-lg-6 border-right">
+            <div className="drop-title">
+              <h4 className="mb-4">{dropTitle}</h4>
+              <p>{dropDescOne}</p>
+              <p className="mb-4">{dropDescTwo}</p>
+            </div>
+            <div className="auction-time">
+              <h5>{auctionTitle}</h5>
+              <NFTCounter time={auctionTime} />
+            </div>
+            <div class="auction-main">
+              <div class="auction-one">
+                <p class="heading-S">Minimum Price</p>
+                <h1>TBA</h1>
+              </div>
+              <div class="auction-two">
+                <p class="heading-S">{editionTitle}</p>
+                <h1>{editionType} </h1>
+              </div>
+            </div>
+            <div className="additional-perks">
+              <h5>{additional}</h5>
+              <p>{additionalDesc}</p>
             </div>
           </div>
-          <div className="row drop_card">
-            <div className="col-lg-6 border-right">
-              <div className="drop-title">
-                <h4 className="mb-4">{dropTitle}</h4>
-                <p>{dropDescOne}</p>
-                <p className="mb-4">{dropDescTwo}</p>
-              </div>
-              <div className="auction-time">
-                <h5>{auctionTitle}</h5>
-                <NFTCounter time={auctionTime} />
-              </div>
-              <div class="auction-main">
-                <div class="auction-one">
-                  <p class="heading-S">Minimum Price</p>
-                  <h1>TBA</h1>
-                </div>
-                <div class="auction-two">
-                  <p class="heading-S">{editionTitle}</p>
-                  <h1>{editionType} </h1>
-                </div>
-              </div>
-              <div className="additional-perks">
-                <h5>{additional}</h5>
-                <p>{additionalDesc}</p>
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="drop-card-post">
-                <Image src={img} role="button" onClick={handleClick} />
-                <div class="learnMore">
-                  {" "}
-                  <a href=""  onClick={handleClick}>Place Your Bid Right Now!</a>{" "}
-                </div>
+          <div className="col-lg-6">
+            <div className="drop-card-post">
+              <Image src={img} role="button" onClick={handleClick} />
+              <div class="learnMore">
+                {" "}
+                <a href="" onClick={handleClick}>
+                  Place Your Bid Right Now!
+                </a>{" "}
               </div>
             </div>
           </div>
         </div>
-     
+      </div>
     </>
   );
 };
