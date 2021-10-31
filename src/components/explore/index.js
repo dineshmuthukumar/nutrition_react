bb3e705db9844b1dd966fad6d5f177dcab6c8483
suganth <suganth@onclouderp.com>
2021-10-31 22:27:20 +0530

    content loader

--- src/components/explore/index.js
+++ src/components/explore/index.js
@@ -1,13 +1,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import ContentLoader from "react-content-loader";
 
 import ExploreCard from "./explore-card";
 import ExploreTitle from "./explore-title";
 
 import "./style.scss";
 
-const Explore = ({ list = [], handleClick, hasMore }) => {
+const Explore = ({ list = [], handleClick, hasMore, loading, loadingMore }) => {
   const { name } = useParams();
   const [explore, setExplore] = useState({
     title: "",
@@ -18,17 +19,17 @@ const Explore = ({ list = [], handleClick, hasMore }) => {
     if (name.includes("Madhushala")) {
       setExplore({
         title: "Madhushala NFTs",
-        description: `Madhushala is a poetic confluence of romance and philosophy, penned by Amit-Ji’s dad Shri. Harivansh Rai Bachchan!        `,
+        description: `Madhushala is a poetic confluence of romance and philosophy, penned by Amit-Ji's dad Shri. Harivansh Rai Bachchan!        `,
       });
     } else if (name.includes("Posters")) {
       setExplore({
-        title: "Autographed Physical Posters",
-        description: `Now you can own a video of Amitabh signing an original movie poster of his iconic movies, hand-painted by a few authentic artists whose work will amaze you in every way. The cult value of their masterpieces is a statement of ethnicity, authenticity, Indian cinema's legacy, and beyond.`,
+        title: "Poster Autographing Moments",
+        description: `Now you can own a video of Amitabh signing an original movie poster of his iconic movies, hand-painted by a few authentic artists whose work will amaze you in every way. The winner of these NFTs also gets a physical poster autographed by the Legend Amitabh Bachchan himself.`,
       });
     } else {
       setExplore({
         title: "BigB Punks and NFT Arts",
-        description: `Let's admit it! Amitabh, in every avatar, has been a success! Be it the classic ‘Angry Young Man', or the modern French-bearded Godfather with his signature 'DEVIYON aur SAJJANO', or the Twitter personality who numbers his Tweets, the BigB can never be off trends!`,
+        description: `Let's admit it! Amitabh, in every avatar, has been a success! Be it the classic 'Angry Young Man', or the modern French-bearded Godfather with his signature 'DEVIYON aur SAJJANO', or the Twitter personality who numbers his Tweets, the BigB can never be off trends!`,
       });
     }
   }, [name]);
@@ -73,55 +74,67 @@ const Explore = ({ list = [], handleClick, hasMore }) => {
                 </div>
               </div>
               <div className="row gutters-20">
-                {list.map((nft) => {
-                  let label = "",
-                    time,
-                    isEnded = false,
-                    isStarted = false,
-                    bidBuyValue = 0;
+                {loading ? (
+                  <ActivityList />
+                ) : (
+                  <>
+                    {list.map((nft) => {
+                      let label = "",
+                        time,
+                        isEnded = false,
+                        isStarted = false,
+                        bidBuyValue = 0;
 
-                  if (new Date(nft.auction_start_time) > new Date(nft.time)) {
-                    label = "Starting in";
-                    time = nft.auction_start_time;
-                  } else if (
-                    new Date(nft.auction_end_time) > new Date(nft.time)
-                  ) {
-                    label = "Ends in";
-                    time = nft.auction_end_time;
-                    isStarted = true;
-                  } else {
-                    time = nft.auction_end_time;
-                    label = "Ended at";
-                    isEnded = true;
-                  }
+                      if (
+                        new Date(nft.auction_start_time) > new Date(nft.time)
+                      ) {
+                        label = "Starting in";
+                        time = nft.auction_start_time;
+                      } else if (
+                        new Date(nft.auction_end_time) > new Date(nft.time)
+                      ) {
+                        label = "Ends in";
+                        time = nft.auction_end_time;
+                        isStarted = true;
+                      } else {
+                        time = nft.auction_end_time;
+                        label = "Ended at";
+                        isEnded = true;
+                      }
 
-                  if (nft.nft_type === "erc721") {
-                    bidBuyValue = nft.minimum_bid;
-                  } else {
-                    bidBuyValue = nft.buy_amount;
-                  }
+                      if (nft.nft_type === "erc721") {
+                        bidBuyValue = nft.minimum_bid;
+                      } else {
+                        bidBuyValue = nft.buy_amount;
+                      }
 
-                  return (
-                    <ExploreCard
-                      slug={nft.slug}
-                      nft={nft}
-                      isStarted={isStarted}
-                      isEnded={isEnded}
-                      time={time}
-                      label={label}
-                      title={nft.name}
-                      bidPrice={bidBuyValue}
-                      desc={nft.description}
-                      nftType={nft.nft_type}
-                    />
-                  );
-                })}
+                      return (
+                        <ExploreCard
+                          slug={nft.slug}
+                          nft={nft}
+                          isStarted={isStarted}
+                          isEnded={isEnded}
+                          time={time}
+                          label={label}
+                          title={nft.name}
+                          bidPrice={bidBuyValue}
+                          desc={nft.description}
+                          nftType={nft.nft_type}
+                        />
+                      );
+                    })}
+                  </>
+                )}
               </div>
               {hasMore && (
                 <div className="row mb-5">
                   <div className="col-md-12 text-center">
-                    <button className="load_more" onClick={handleClick}>
-                      Load More
+                    <button
+                      className="load_more"
+                      disabled={loadingMore}
+                      onClick={handleClick}
+                    >
+                      {loadingMore ? "Loading..." : "Load More"}
                     </button>
                   </div>
                 </div>
@@ -133,5 +146,21 @@ const Explore = ({ list = [], handleClick, hasMore }) => {
     </>
   );
 };
+
+const ActivityList = (props) => (
+  <ContentLoader
+    viewBox="0 0 900 400"
+    width={"100%"}
+    height={"100%"}
+    backgroundColor="#f5f5f5"
+    foregroundColor="#dbdbdb"
+    className="mt-3"
+    {...props}
+  >
+    <rect x="12" y="5" rx="2" ry="2" width="280" height="300" />
+    <rect x="308" y="5" rx="2" ry="2" width="280" height="300" />
+    <rect x="600" y="5" rx="2" ry="2" width="280" height="300" />
+  </ContentLoader>
+);
 
 export default Explore;
