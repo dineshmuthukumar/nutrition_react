ed19c3aa8a4a6c3a68f4a84c0e1d19bb486da53f
suganth <suganth@onclouderp.com>
2021-11-01 01:25:34 +0000

    time update

--- src/components/new-drops-temp2/index.js
+++ src/components/new-drops-temp2/index.js
@@ -51,11 +51,11 @@ const NewDropsTemp2 = ({
   const [vEmail2, setVEmail2] = useState();
   const [active, setActive] = useState();
 
-  const date = "Nov 01, 2021 00:55:00";
+  const date = "Nov 01, 2021 01:40:00";
 
   const [atime, setAtime] = useState(date);
 
-  const end_date = "Nov 01, 2021 00:58:00";
+  const end_date = "Nov 01, 2021 02:30:00";
 
   useEffect(() => {
     var offset = new Date().getTimezoneOffset();
