6d9cfa1b39cbed36baeae9164128929779940770
vijayguardian <88785341+vijayguardian@users.noreply.github.com>
2021-11-02 16:09:35 +0530

    Loot Winner page design

--- src/components/loot-nft-own/index.js
+++ src/components/loot-nft-own/index.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import dayjs from "dayjs";
 
 import userImg from "../../images/user_1.png";
+import winnerImg from "../../images/success.png";
 import { currencyFormat } from "../../utils/common";
 import "./style.scss";
 
@@ -14,12 +15,26 @@ const LootNFTOwn = ({ nft }) => {
         <div className="winner-title">
           <div className="winner-text">HURRAY!</div>
         </div>
-
-        <div className="winner-user-details">
-          <img alt="" src={user ? user.avatar_url : userImg} />
-          <div className="winner-id">{user && `@${user.first_name}${user.last_name}`}</div>
+        <div className="winner_details">
+          <div className="winner-user-details">
+            <img alt="" src={winnerImg} />
+            <div className="winner-id">
+              {user && `@${user.first_name}${user.last_name}`}
+            </div>
+          </div>
+          <div className="nft-lastbid-details">
+            <div className="lastbid-left">
+              <div className="lastbid-title">Price</div>
+              <div className="lastbid-value">{currencyFormat(10, "USD")}</div>
+            </div>
+            <div className="lastbid-right">
+              <div className="lastbid-date-title">You Own</div>
+              <div className="lastbid-date-value">
+                {nft.nft_type === "erc721" ? "1 of 1" : nft.total_user_buys}
+              </div>
+            </div>
+          </div>
         </div>
-
         {/* <div className="nft-sold-details">
           <div className="sold-for">
             <div className="sold-for-title">NFT sold for</div>
@@ -32,19 +47,6 @@ const LootNFTOwn = ({ nft }) => {
             </div>
           </div>
         </div> */}
-
-        <div className="nft-lastbid-details">
-          <div className="lastbid-left">
-            <div className="lastbid-title">Price</div>
-            <div className="lastbid-value">{currencyFormat(10, "USD")}</div>
-          </div>
-          <div className="lastbid-right">
-            <div className="lastbid-date-title">You Own</div>
-            <div className="lastbid-date-value">
-              {nft.nft_type === "erc721" ? "1 of 1" : nft.total_user_buys}
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
