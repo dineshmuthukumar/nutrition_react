e97fe75bfca1eaaabe7d69516eae9067d38cfeaa
suganthg <suganth@onclouderp.com>
2022-01-12 17:58:48 +0530

    cookie change

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,78 +1,57 @@
-import Cookies from "universal-cookie";
+import cookie from "react-cookies";
 
 const cookie_token = "bl_base_user_token";
 
-const cookies = new Cookies();
-
 export const setCookiesByName = (name, input) => {
-  if (process.env.REACT_APP_ENVIRONMENT === "local") {
-    cookies.set(name, input);
-  } else {
-    cookies.set(name, input, {
-      domain: process.env.REACT_APP_COOKIE_DOMAIN_NAME,
-    });
-  }
+  const expires = new Date();
+  expires.setDate(Date.now() + 1000 * 60 * 60 * 24 * 14);
+  const maxAge = 365 * 24 * 60 * 60;
+  const path = "/";
+
+  cookie.save(name, input, {
+    path,
+    expires,
+    maxAge,
+    domain:
+      process.env.REACT_APP_ENVIRONMENT === "local"
+        ? "localhost"
+        : process.env.REACT_APP_COOKIE_DOMAIN_NAME,
+    secure: true,
+  });
 };
 
 export const setCookies = (input) => {
-  if (process.env.REACT_APP_ENVIRONMENT === "local") {
-    cookies.set(cookie_token, input);
-  } else {
-    cookies.set(cookie_token, input, {
-      domain: process.env.REACT_APP_COOKIE_DOMAIN_NAME,
-    });
-  }
+  const expires = new Date();
+  expires.setDate(Date.now() + 1000 * 60 * 60 * 24 * 14);
+  const maxAge = 365 * 24 * 60 * 60;
+  const path = "/";
+
+  cookie.save(cookie_token, input, {
+    path,
+    expires,
+    maxAge,
+    domain:
+      process.env.REACT_APP_ENVIRONMENT === "local"
+        ? "localhost"
+        : process.env.REACT_APP_COOKIE_DOMAIN_NAME,
+    secure: true,
+  });
 };
 
 export const removeCookiesByName = (name) => {
-  if (process.env.REACT_APP_ENVIRONMENT === "local") {
-    cookies.remove(name, {
-      domain: process.env.REACT_APP_COOKIE_DOMAIN_NAME,
-      path: "/",
-    });
-  } else {
-    cookies.remove(name, {
-      domain: process.env.REACT_APP_COOKIE_DOMAIN_NAME,
-      path: "/",
-    });
-  }
+  cookie.remove(name, {
+    path: "/",
+    domain: process.env.REACT_APP_COOKIE_DOMAIN_NAME,
+  });
 };
 
 export const getCookies = () => {
-  return cookies.get(cookie_token);
+  return cookie.load(cookie_token);
 };
 
 export const removeCookies = () => {
-  if (process.env.REACT_APP_ENVIRONMENT === "local") {
-    cookies.remove(cookie_token, {
-      domain: process.env.REACT_APP_COOKIE_DOMAIN_NAME,
-      path: "/",
-    });
-  } else {
-    cookies.remove(cookie_token, {
-      domain: process.env.REACT_APP_COOKIE_DOMAIN_NAME,
-      path: "/",
-    });
-  }
-
-  (function () {
-    var cookies = document.cookie.split("; ");
-    for (var c = 0; c < cookies.length; c++) {
-      var d = window.location.hostname.split(".");
-      while (d.length > 0) {
-        var cookieBase =
-          encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) +
-          "=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=" +
-          d.join(".") +
-          " ;path=";
-        var p = window.location.pathname.split("/");
-        document.cookie = cookieBase + "/";
-        while (p.length > 0) {
-          document.cookie = cookieBase + p.join("/");
-          p.pop();
-        }
-        d.shift();
-      }
-    }
-  })();
+  cookie.remove(cookie_token, {
+    path: "/",
+    domain: process.env.REACT_APP_COOKIE_DOMAIN_NAME,
+  });
 };
