483917a52e3a1adb5ca4ad4f7cd442f598c4bf4d
Vignesh R <vignesh.r@blockchainappfactory.com>
2022-01-26 16:28:36 +0530

    Recently Sold in dashboard and separate page

---
+++ src/components/recently-sold-list/index.js
@@ -0,0 +1,127 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect, useState } from "react";
+import ContentLoader from "react-content-loader";
+
+import { topTradesApi } from "../../api/methods";
+import NFTCard from "../nft-card";
+import sample from "../../images/sampleNFT.jpg";
+import "./style.scss";
+
+const RecentlySoldList = () => {
+  const [page, setPage] = useState(1);
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [hasNext, setHasNext] = useState(false);
+  const [loadingMore, setLoadingMore] = useState(false);
+
+  useEffect(() => {
+    topTradesList(page);
+  }, []);
+
+  const topTradesList = async (page) => {
+    try {
+      page === 1 && setLoading(true);
+      setLoadingMore(true);
+      let response = await topTradesApi({ page });
+      setList(response.data.data.nfts);
+      setHasNext(response.data.data.next_page);
+      page === 1 && setLoading(false);
+      setLoadingMore(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const showMoreTopTradesList = async (page) => {
+    try {
+      page === 1 && setLoading(true);
+      setLoadingMore(true);
+      let response = await topTradesApi({ page });
+      setList([...list, ...response.data.data.nfts]);
+      setHasNext(response.data.data.next_page);
+      page === 1 && setLoading(false);
+      setLoadingMore(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const fetchMore = () => {
+    if (hasNext) {
+      showMoreTopTradesList(page + 1);
+      setPage(page + 1);
+    }
+  };
+
+  return (
+    <>
+      <section className="explore-nft-section">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="sec-heading d-flex align-items-center mb-5 explore-heading">
+                <span className="me-4 text-nowrap">Recently Sold</span>
+              </div>
+
+              {!loading ? (
+                <div className="row">
+                  {list.length > 0 ? (
+                    list.map((nft, i) => (
+                      <div
+                        key={`list-nft-${i}`}
+                        className="col-xl-3 col-lg-4 col-md-6 col-sm-6"
+                      >
+                        <NFTCard nft={nft} key={i} image={sample} recentSold />
+                      </div>
+                    ))
+                  ) : (
+                    <div className="col-12 text-center mb-5">
+                      <h3 className="my-3">No Records Found!</h3>
+                    </div>
+                  )}
+
+                  {!loading && loadingMore && <NFTCardLoader />}
+
+                  {hasNext && (
+                    <div className="row mb-5">
+                      <div className="col-md-12 text-center">
+                        <button
+                          className="load_more"
+                          disabled={loadingMore}
+                          onClick={fetchMore}
+                        >
+                          {loadingMore ? "Loading..." : "Load More"}
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <NFTCardLoader />
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+const NFTCardLoader = (props) => (
+  <ContentLoader
+    viewBox="0 50 900 300"
+    width={"100%"}
+    height={"100%"}
+    backgroundColor="#f5f5f5"
+    foregroundColor="#dbdbdb"
+    className="mt-1"
+    {...props}
+  >
+    <rect x="0" y="5" rx="2" ry="2" width="218" height="280" />
+    <rect x="228" y="5" rx="2" ry="2" width="218" height="280" />
+    <rect x="456" y="5" rx="2" ry="2" width="218" height="280" />
+    <rect x="684" y="5" rx="2" ry="2" width="218" height="280" />
+  </ContentLoader>
+);
+
+export default RecentlySoldList;
