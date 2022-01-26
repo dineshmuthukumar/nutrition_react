244c1d69c0d94c3a653b0c47193df63ce14fc744
Vignesh R <vignesh.r@blockchainappfactory.com>
2022-01-26 17:08:09 +0530

    temp fix for zero in top-seller

--- src/components/top-sellers/seller.js
+++ src/components/top-sellers/seller.js
@@ -11,38 +11,40 @@ const Seller = ({ index, seller, image }) => {
   const { user } = useSelector((state) => state.user.data);
   return (
     <>
-      <div className="col-xl-3 col-lg-4 col-sm-6">
-        <article className="user-card">
-          <h5 className="user-count"> {index + 1} </h5>
-          <figure>
-            <img
-              src={
-                !seller.private && seller.avatar_url
-                  ? seller.avatar_url
-                  : user?.slug === seller.slug && seller.avatar_url
-                  ? seller.avatar_url
-                  : userImg
-              }
-              alt="userImge"
-              role={"button"}
-              onClick={() => history.push(`/seller/${seller.slug}/details`)}
-            />
-            <IoIosCheckmark className="user-verify" />
-          </figure>
-          <div className="user-content">
-            <h3
-              className="user-name"
-              role={"button"}
-              onClick={() => history.push(`/seller/${seller.slug}/details`)}
-            >
-              {seller.user_name}
-            </h3>
-            <h4 className="user-price">
-              {currencyFormat(seller.sold_total, "USD")}
-            </h4>
-          </div>
-        </article>
-      </div>
+      {parseFloat(seller?.sold_total) > 0 && (
+        <div className="col-xl-3 col-lg-4 col-sm-6">
+          <article className="user-card">
+            <h5 className="user-count"> {index === 0 ? index + 1 : index} </h5>
+            <figure>
+              <img
+                src={
+                  !seller.private && seller.avatar_url
+                    ? seller.avatar_url
+                    : user?.slug === seller.slug && seller.avatar_url
+                    ? seller.avatar_url
+                    : userImg
+                }
+                alt="userImge"
+                role={"button"}
+                onClick={() => history.push(`/seller/${seller.slug}/details`)}
+              />
+              <IoIosCheckmark className="user-verify" />
+            </figure>
+            <div className="user-content">
+              <h3
+                className="user-name"
+                role={"button"}
+                onClick={() => history.push(`/seller/${seller.slug}/details`)}
+              >
+                {seller.user_name}
+              </h3>
+              <h4 className="user-price">
+                {currencyFormat(seller.sold_total, "USD")}
+              </h4>
+            </div>
+          </article>
+        </div>
+      )}
     </>
   );
 };
