c907c29ec2209e5d62ddca8ce2dcfba6c16f856f
ManiMohan R <manimohan.r@blockchainappfactory.com>
2022-01-27 19:00:27 +0530

    recently sold Filter

--- src/components/recently-sold-list/index.js
+++ src/components/recently-sold-list/index.js
@@ -1,18 +1,91 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from "react";
 import ContentLoader from "react-content-loader";
+import { useHistory, useParams } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
+import { FaCheckCircle } from "react-icons/fa";
+import { nftRecentlySoldApi } from "../../api/methods";
 
 import { topTradesApi } from "../../api/methods";
 import NFTCard from "../nft-card";
 import sample from "../../images/sampleNFT.jpg";
 import "./style.scss";
+import useQuery from "../../hook/useQuery";
+import { BiCaretDown } from "react-icons/bi";
 
 const RecentlySoldList = () => {
+  const history = useHistory();
   const [page, setPage] = useState(1);
   const [list, setList] = useState([]);
   const [loading, setLoading] = useState(false);
   const [hasNext, setHasNext] = useState(false);
   const [loadingMore, setLoadingMore] = useState(false);
+  const query = useQuery();
+  const { slug } = useParams();
+
+  const [filter, setFilter] = useState({
+    sort: [
+      {
+        name: "Recently Sold",
+        value: "recently_sold",
+        checked: true,
+      },
+      {
+        name: "Price - High to Low",
+        value: "price_desc",
+        checked: false,
+      },
+      {
+        name: "Price - Low to High",
+        value: "price_asc",
+        checked: false,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    const sort_filters = query.get("sort")
+      ? query.get("sort")
+      : "recently_sold";
+
+    const info = { ...filter };
+
+    info.sort = filter.sort.map((obj) => ({
+      ...obj,
+      checked: sort_filters ? sort_filters === obj.value : false,
+    }));
+    showAllFilteredNFTs(1, sort_filters);
+
+    setFilter(info);
+  }, [slug, query]);
+
+  const showAllNFTs = async (page, sort = "recently_sold") => {
+    try {
+      page === 1 && setLoading(true);
+      setLoadingMore(true);
+      let response = await nftRecentlySoldApi(page, sort);
+      setList([...list, ...response.data.data.nfts]);
+      setHasNext(response.data.data.next_page);
+      page === 1 && setLoading(false);
+      setLoadingMore(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const showAllFilteredNFTs = async (page, sort = "recently_sold") => {
+    try {
+      page === 1 && setLoading(true);
+      setLoadingMore(true);
+      let response = await nftRecentlySoldApi(page, sort);
+      setList(response.data.data.nfts);
+      setHasNext(response.data.data.next_page);
+      page === 1 && setLoading(false);
+      setLoadingMore(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     topTradesList(page);
@@ -48,9 +121,44 @@ const RecentlySoldList = () => {
 
   const fetchMore = () => {
     if (hasNext) {
-      showMoreTopTradesList(page + 1);
+      const sort_filters = query.get("sort")
+        ? query.get("sort")
+        : "recently_sold";
+
+      showAllNFTs(page + 1, sort_filters);
+      // showMoreTopTradesList(page + 1);
       setPage(page + 1);
     }
+  };
+
+  const ShowAllSort = React.forwardRef(({ onClick }, ref) => (
+    <div
+      className="filter-drop-sort-btn"
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {filter.sort.find((obj) => obj.checked === true)?.name
+        ? `Sort By: ${filter.sort.find((obj) => obj.checked === true).name}`
+        : "Sort By"}
+      <BiCaretDown />
+    </div>
+  ));
+
+  const handleSortNFT = (input) => {
+    const sort_exist = input.value;
+
+    let query_string = "";
+
+    if (sort_exist) {
+      query_string += query_string
+        ? `&sort=${sort_exist}`
+        : `sort=${sort_exist}`;
+    }
+
+    history.push(`/nfts/recently-sold?${query_string}`);
   };
 
   return (
@@ -61,6 +169,34 @@ const RecentlySoldList = () => {
             <div className="col-sm-12">
               <div className="sec-heading d-flex align-items-center mb-5 explore-heading">
                 <span className="me-4 text-nowrap">Recently Sold</span>
+                <span className="d-flex justify-content-end mt-2 w-100 filter-blocks">
+                  <div className="filt-flex-box">
+                    <Dropdown>
+                      <Dropdown.Toggle
+                        align="start"
+                        drop="start"
+                        as={ShowAllSort}
+                      ></Dropdown.Toggle>
+
+                      <Dropdown.Menu align="start">
+                        {filter.sort.map((obj, i) => (
+                          <Dropdown.Item
+                            key={`nft${i}`}
+                            as="button"
+                            onClick={() => handleSortNFT(obj)}
+                          >
+                            <FaCheckCircle
+                              color={obj.checked ? "green" : "#ccc"}
+                              className="mb-1 me-2"
+                              size={17}
+                            />{" "}
+                            {obj.name}
+                          </Dropdown.Item>
+                        ))}
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
+                </span>
               </div>
 
               {!loading ? (
