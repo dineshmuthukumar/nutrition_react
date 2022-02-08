210aab5de73549afb4d09b3492d4e65969a1e3eb
suganthg <suganth@onclouderp.com>
2022-02-08 18:31:19 +0530

    tracking fsz from category explore

--- src/pages/explore-list.js
+++ src/pages/explore-list.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
+import useQuery from "../hook/useQuery";
 import Explore from "../components/explore";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import { nftCategoryDetailApi } from "../api/methods";
+import { setCookiesByName } from "../utils/cookies";
 
 const ExploreList = () => {
   const { slug } = useParams();
   const [categoryDetail, setCategoryDetail] = useState({});
   const [loading, setLoading] = useState(false);
+  let query = useQuery();
+  const fsz = query.get("fsz");
 
   useEffect(() => {
+    if (fsz) {
+      sessionStorage.setItem("fsz", fsz);
+      setCookiesByName("source", fsz);
+    }
+
     nftCategoryDetail(slug);
   }, []);
 
