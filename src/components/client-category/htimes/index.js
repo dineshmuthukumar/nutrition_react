b2c68cd8e1f0d7464920a9170ae61585672dbb50
suganthg <suganth@onclouderp.com>
2022-02-17 16:40:41 +0530

    added client category - htimes

---
+++ src/components/client-category/htimes/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Footer from "../../footer";
+import Header from "../../header";
+
+import "./style.scss";
+
+const HTimes = () => {
+  return (
+    <div>
+      <Header />
+      hello <br />
+      hello <br />
+      hello <br />
+      hello <br />
+      hello <br />
+      hello <br />
+      <Footer />
+    </div>
+  );
+};
+
+export default HTimes;
