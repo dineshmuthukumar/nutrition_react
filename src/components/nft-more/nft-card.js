371bfe3afec72776a97f334eb485b32553e93040
Vignesh R <vignesh.r@blockchainappfactory.com>
2022-04-30 14:43:01 +0530

    heart hide in nft card

--- src/components/nft-more/nft-card.js
+++ src/components/nft-more/nft-card.js
@@ -166,11 +166,11 @@ const CollectionCard = ({ nft, recentSold = false, favouriteNFT = false }) => {
       />
 
       <div className="top-content-title">
-        <div className="heart_box">
-          <div className="svg_size filled_heart_icon"></div>
+        {/* <div className="heart_box">
+          <div className="svg_size filled_heart_icon"></div> */}
 
-          {/* <div className="svg_size heart_icon"></div> */}
-        </div>
+        {/* <div className="svg_size heart_icon"></div> */}
+        {/* </div> */}
         <div>
           <div className="more-nft-title">{nft?.name}</div>
           {nft?.owner_name && (
