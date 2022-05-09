a69d329d631ff43d1f9e32b206aa8fb3de17af94
Vignesh R <vignesh.r@blockchainappfactory.com>
2022-05-09 18:23:41 +0530

    trending NFTs

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,6 +5,7 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import Banner from "../components/banner";
 import LiveAuctions from "../components/live-auctions";
+import Trending from "../components/trending";
 import HotCollections from "../components/hot-collections";
 import TopBuyers from "../components/top-buyers";
 import TopSellers from "../components/top-sellers";
@@ -104,6 +105,7 @@ const Home = () => {
         {/* <NewDropSection /> */}
         <CollectionList />
         <LiveAuctions />
+        <Trending />
         <RecentlySoldNFT />
 
         {/* {favList.length > 0 && (
