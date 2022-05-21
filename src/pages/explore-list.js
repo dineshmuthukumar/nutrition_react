09cf1967f7d6f0975a2154bc7579e3b401f9a0ee
krishnaguru09 <krishna.guru@guardianlink.io>
2022-05-21 18:21:52 +0530

    Added meta for category pages

--- src/pages/explore-list.js
+++ src/pages/explore-list.js
@@ -9,7 +9,7 @@ import { nftCategoryDetailApi } from "../api/methods";
 import { setCookiesByName } from "../utils/cookies";
 
 const ExploreList = () => {
-  const { cSlug } = useParams();
+  const { cSlug ,category } = useParams();
   const [categoryDetail, setCategoryDetail] = useState({});
   const [loading, setLoading] = useState(false);
   let query = useQuery();
@@ -37,7 +37,17 @@ const ExploreList = () => {
 
   return (
     <>
-      <Header bgImage />
+      <Header 
+        bgImage 
+        title={
+          category === "cricket-player-nfts" 
+          ? "Meta Cricket League Players | Jump.Trade" 
+          : "Meta Cricket League Signed Bat | Jump.Trade"} 
+        description={
+          category === "cricket-player-nfts" 
+          ? "Explore the stunning collection of Meta Cricket League player NFTs here. Buy your favourite players and gear up to play in the cricket metaverse." 
+          : "We bring you the Meta Cricket League Signed bat NFT collections. Find out signed bats of legendary players here. Buy them and power up your game in the cricket metaverse." } 
+      />
       <Explore categoryDetail={categoryDetail} slug={categoryDetail?.slug} />
       <Footer />
     </>
