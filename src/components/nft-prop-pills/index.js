8349a35aea1249a3b59f38615862275cd05a0367
Nimal Raj <nimal@blockchainappfactory.com>
2022-05-24 18:26:12 +0530

    Properties UI updated

--- src/components/nft-prop-pills/index.js
+++ src/components/nft-prop-pills/index.js
@@ -3,12 +3,15 @@ import "./style.scss";
 
 const NFTPropPills = ({ property, propertyType }) => {
   return (
-    <div className={`chain-pills rounded-pill border border-dark`}>
+    <div
+      className={`chain-pills rounded-pill border border-dark property-pill-box`}
+    >
       <div className="first">
         <div className="second rounded-pill border border-dark">
           {propertyType}
         </div>
-        {property}
+        <span className="pill-title">{property}</span>
+        <span className="pill-description">100% have this trait</span>
       </div>
     </div>
   );
