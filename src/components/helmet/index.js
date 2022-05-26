64483ea6ac65c0c0316df07a0b8974f082818382
krishnaguru09 <krishna.guru@guardianlink.io>
2022-05-26 19:58:06 +0530

    Removed default title, description and image from AppHelmet component

--- src/components/helmet/index.js
+++ src/components/helmet/index.js
@@ -1,31 +1,26 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-const AppHelmet = ({
-  title = "NFT Marketplace | Cricket NFT Marketplace | Buy & Sell Cricket NFTs",
-  description = "Jump.trade is the most secured NFT marketplace where you can buy & sell rare cricket NFTs. Discover, collect, and trade authentic cricket player NFTs on our NFT gaming marketplace. Sign up now!",
-  image = "https://cdn.guardianlink.io/product-hotspot/images/og-image_jt.jpg",
-  hideCanonical = false,
-}) => {
+const AppHelmet = ({ title, description, image, hideCanonical = false }) => {
   return (
     <Helmet>
-      <title>{title} </title>
-      <meta name="description" content={description} />
+      {title && <title>{title} </title>}
+      {description && <meta name="description" content={description} />}
 
-      <meta property="og:title" content={title} />
+      {title && <meta property="og:title" content={title} />}
       <meta property="og:type" content="website" />
       <meta property="og:url" content={window.location.href} />
-      <meta property="og:image" content={image} />
+      {image && <meta property="og:image" content={image} />}
       <meta property="og:site_name" content="Jump.Trade" />
-      <meta property="og:description" content={description} />
+      {description && <meta property="og:description" content={description} />}
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={window.location.href} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:image" content={image} />
-      <meta name="twitter:description" content={description} />
+      {title && <meta name="twitter:title" content={title} />}
+      {image && <meta name="twitter:image" content={image} />}
+      {description && <meta name="twitter:description" content={description} />}
 
-      <meta itemprop="image" content={image} />
+      {image && <meta itemprop="image" content={image} />}
       {!hideCanonical && (
         <link rel="canonical" href={window.location.href.split(/[?#]/)[0]} />
       )}
