5cc3366339a6b0ba909710f7c84f6b8ccb8af6d9
Abishek_Raja <abishek.r@tranxitgroup.com>
2022-06-01 18:54:13 +0530

    privacy policy & terms header footer removed

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -2,17 +2,20 @@ import React from "react";
 import Header from "../components/header";
 import TermsAndConditions from "../components/terms-and-conditions";
 import Footer from "../components/footer";
+import useQuery from "../hook/useQuery";
 
 const Terms = () => {
+  const query = useQuery();
+  const hide = query.get("hide");
   return (
     <>
-      <Header
+      {!hide && <Header
         bgImage
         title="Terms And Conditions | T&C | Jump.Trade"
         description="The Jump.trade NFT marketplace's terms and conditions are  in the interest of our community, creators, and our business."
-      />
+      />}
       <TermsAndConditions />
-      <Footer />
+      {!hide && <Footer />}
     </>
   );
 };
