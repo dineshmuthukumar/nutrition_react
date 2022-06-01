af290520b330ec25f44a120d9c9de03f27b3d15a
Abishek_Raja <abishek.r@tranxitgroup.com>
2022-06-01 18:54:13 +0530

    privacy policy & terms header footer removed

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -2,17 +2,20 @@ import React from "react";
 import Header from "../components/header";
 import PrivacyComponent from "../components/privacy-policy";
 import Footer from "../components/footer";
+import useQuery from "../hook/useQuery";
 
 const PrivacyPolicy = () => {
+  let query = useQuery();
+  const hide = query.get("hide");
   return (
     <>
-      <Header
+      {!hide && <Header
         bgImage
         title="Privacy Policy | Jump.Trade"
         description="The Jump.trade NFT marketplace is committed to protect the privacy of all user information."
-      />
+      />}
       <PrivacyComponent />
-      <Footer />
+      {!hide && <Footer />}
     </>
   );
 };
