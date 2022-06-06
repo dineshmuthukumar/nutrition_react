922990e3f556e9c3a1c674ae7206523fb21e8007
Harish <hariharan.g@tranxitgroup.com>
2022-06-06 19:46:14 +0530

    detail page

--- src/components/blogs/detail.js
+++ src/components/blogs/detail.js
@@ -1,7 +1,172 @@
 import React from "react";
 
 const BlogDetail = () => {
-  return <div>BlogDetail</div>;
+  return (
+    <div>
+      <section class="blog-content p-80">
+        <div class="container">
+          <div class="row">
+            <div class="title-head text-center">
+              <h3>
+                <a href="">Blog</a>
+                <a href="">Case Study</a>
+              </h3>
+              <h2>Meta Cricket League NFT Collection – Jump.trade</h2>
+              <h5 class="b_date">June 3,2022</h5>
+            </div>
+          </div>
+          <div class="row">
+            <div class="border-line"></div>
+          </div>
+          <div class="row">
+            <div class="col-md-10 col-lg-10 col-sm-10 col-xs-12 offset-md-1 offset-lg-1 offset-sm-1">
+              <div class="right-blog-cnt">
+                <p>
+                  ShareShare Jump.trade is the new premium marketplace from
+                  GuardianLink that will predominantly feature gaming NFT drops.
+                  This page will focus on information on gaming, other
+                  international content and a lot more NFT oriented whereabouts.
+                  Join us in creating this revolution and breaking records
+                  together!
+                </p>
+                <p>Meta Cricket League NFT Collection</p>
+                <p>
+                  On 22nd April’22, Jump.trade, as its initial launch, dropped
+                  the World’s First Cricket Game NFT Collection. Creating a
+                  record of 55,000 NFTs getting sold in just nine minutes,
+                  amounting to about 0.009 seconds for every NFT being sold,
+                  making this one of the fastest and the most prolific NFT drops
+                  ever in the history of gaming and NFTs.
+                </p>
+                <p>What is the collection about</p>
+                <p>
+                  The NFT drop was a combination of cricket players’ NFT avatars
+                  and exclusive cricket bat NFTs. The cricket bats are
+                  authenticated and signed by some of the legendary cricketers
+                  from India and prominent cricket names across the globe.
+                </p>
+                <p>
+                  The collections, going with the GuardianLink legacy, were
+                  bundled in a blind purchase called the Super LootBox.
+                </p>
+                <p>
+                  However, the major USP of the NFT collection was the series of
+                  signed cricket bat NFTs in 3-D. These 3-D NFTs featured signed
+                  bats of the World Cup-winning team of 1983, both the finalists
+                  of the World Cup of 2003, and the victorious team of the 2011
+                  World Cup. Considering how prized these NFTs were, they were
+                  put up on auctions starting at $25 per collection. Every
+                  collection had three NFTs ( one signed bat NFT, and two player
+                  NFTs.)
+                </p>
+                <p>
+                  These signed bats would not only be highly collectible and
+                  unique but would also have a lot of value in the gameplay as a
+                  part of the roadmap that follows Meta Cricket League, P2E game
+                  in Q2 ’22.
+                </p>
+                <p>The classification:</p>
+                <p>
+                  NFTs would not have varying degrees of demand and rarity if
+                  not for their classification, and the NFTs dropped on
+                  Jump.trade for the game Meta Cricket League also complied with
+                  this industry standard.
+                </p>
+                <p>
+                  The players were classified as rookie, rare, epic,legendary.
+                  These classification categories apply to both bowlers and
+                  batsmen. The bats, depending on the player who has endorsed
+                  them, were classified as rare, super rare, ultra-rare, and
+                  immortal.
+                </p>
+                <p>
+                  This unique game of NFT cricket combined the prowess of the
+                  player, the capacity of the bats, and the skill of the user,
+                  making it an exclusive cricket game where users can use any
+                  combination to upgrade their playing capacity.
+                </p>
+                <p>Pre-booking and its outcomes :</p>
+                <p>
+                  To encourage our collectors and the community, a way to
+                  enhance the drive of our enthusiasts, we opened a pre-booking
+                  option for the first time ever in the history of GuardianLink
+                  NFTs.
+                </p>
+                <p>
+                  The pre-booking met with an overwhelming response receiving
+                  more than 200% of requests. Not for the NFTs earmarked for pre
+                  booking but for the entire NFT collection volume!
+                </p>
+                <p>
+                  On 20th Apr ’22, we revealed that 52,172 Super Loot NFTs have
+                  been pre-booked which is literally double the count of the
+                  entire NFT collection in the drop. In the interest of the open
+                  sale, we had to block only 16,000 Super Loot NFTs in the
+                  pre-booking allocation, presenting 9000 Super Loot NFTs for
+                  the drop on 22nd Apr’22 (5:00 PM IST), for public sale.
+                </p>
+                <p>
+                  About 9000 NFTs were set aside for the day of the actual rock
+                  and based on a specified priority, NFT collections were
+                  allotted to people who pre-booked the NFTs.
+                </p>
+                <p>The Drop day – 22nd Apr’22</p>
+                <p>
+                  As scheduled, the auctions opened at 4:00 PM on 22nd Apr ’22,
+                  and the collection immediately saw a 1000% increase in the bid
+                  price from the base price.
+                </p>
+                <p>
+                  The Super Loot opened at 5:00 PM on the same day, and in
+                  addition to the pre-booking, a total of 55,000 NFTs spread
+                  across 25,000 Super Loot packages were sold in less than nine
+                  minutes. That makes it about one NFT sold every 0.009 seconds.
+                  One of the fastest-selling NFT drops ever!
+                </p>
+                <p>The Treasure Box</p>
+                <p>
+                  If all of this wasn’t rewarding enough, GuardianLink, going
+                  with its legacy of rewarding the community, brought the
+                  Treasure Box again!
+                </p>
+                <p>
+                  The Treasure Box not only included the current allocation of
+                  $50,000 but also included the remnants from the last Treasure
+                  Box, making the total value of the Treasure Box about $52,998.
+                </p>
+                <p>
+                  Every five NFTs would unlock a Treasure Box. This meant that
+                  the more NFTs you hold, the more were your chances of winning
+                  prizes. The best part about the Treasure Box is that it was
+                  not left to probability. Every Treasure Box contains at least
+                  some reward or the other! The box was a true treasure in every
+                  sense, as it unlocked cryptos, some with highest value too.
+                </p>
+                <p>The finishing strike</p>
+                <p>
+                  GuardianLink, through Jump.trade, is bringing you the world’s
+                  first-ever cricket NFT game, and the overwhelming response and
+                  astounding statistics have shown that the game will be
+                  welcomed.
+                </p>
+                <p>
+                  We have more sports-related NFT drops lined up, and we are
+                  sure and hopeful that these NFT drops will make waves just
+                  like every NFT collection has been a success, with the MCL
+                  game and benchmark setting NFT drop Jump.trade is here
+                  creating an ecosystem that will enable enormous possibility in
+                  the future.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="border-line"></div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default BlogDetail;
