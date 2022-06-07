75a5c9a6aabe7d46910ab3ed1a262e681fd40f67
“Harish” <hariharan.g@tranxitgroup.com>
2022-06-07 17:47:47 +0530

    meta title and desc changes

--- src/pages/blog-details.js
+++ src/pages/blog-details.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import BlogDetail from "../components/blogs/detail";
+import {getBlogMetaApi} from "../api/methods";
+import { useParams } from "react-router";
 
 const BlogDetails = () => {
+  const { slug } = useParams();
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  useEffect(() => {
+    metaDetail();
+  }, []);
+  const metaDetail = async () => {
+    try {
+      
+      const GetmetaDetails = await getBlogMetaApi({ slug: slug });
+      setTitle(GetmetaDetails?.data?.json?.og_title);
+      setDescription(GetmetaDetails?.data?.json?.description);
+      
+
+      //console.log(LastAnnouncementData.slice(3));
+    } catch (error) {
+      // setReLoading(false);
+      //toast.error("An unexpected error occured. Please try again  later");
+      console.log(
+        ":rocket: ~ file: index.js ~ line 92 ~ responseGoogle ~ error",
+        error
+      );
+    }
+  };
   return (
     <>
-      <Header bgImage title="Blogs | Jump.Trade" />
+      <Header bgImage title={title}  description={description} />
       <BlogDetail />
       <Footer />
     </>
