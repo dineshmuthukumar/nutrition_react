a922e69a031eec6c83e662fe9a03059df3c2bd27
“dineshmuthukumar” <dineshmuthukumar.g@tranxitgroup.com>
2022-06-07 16:13:33 +0530

    meta tag added in blog

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -6,7 +6,11 @@ import BlogList from "../components/blogs";
 const Blog = () => {
   return (
     <>
-      <Header bgImage title="Blogs | Jump.Trade" />
+      <Header
+        bgImage
+        title="Blogs | Jump.Trade"
+        description="Take a look at the cricket NFTs that got sold recently on the jump.trade NFT marketplace. We know you missed it! Go buy the ones that are live for sale."
+      />
       <BlogList />
       <Footer />
     </>
