dab30a14449cece3a460c343b9bba887dd2149dd
“Harish” <hariharan.g@tranxitgroup.com>
2022-06-08 17:52:12 +0530

    chnages

--- src/components/blogs/announcedetails.js
+++ src/components/blogs/announcedetails.js
@@ -9,6 +9,7 @@ const BlogDetail = () => {
   const [data, setData] = useState();
   const [cate, setCate] = useState();
   const { slug } = useParams();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     blogDetail();
@@ -16,6 +17,7 @@ const BlogDetail = () => {
 
   const blogDetail = async () => {
     try {
+      setLoading(true);
       const blogData = await getBlogCateListApi();
       const CateData = await getBlogCattApi();
       const filteredblogData = blogData?.data?.filter(
@@ -29,6 +31,7 @@ const BlogDetail = () => {
       //console.log(filteredcateData[0]);
       setCate(filteredcateData[0].name);
       //console.log(filteredblogData);
+      setLoading(false);
     } catch (error) {
       // setReLoading(false);
       //toast.error("An unexpected error occured. Please try again  later");
@@ -36,39 +39,52 @@ const BlogDetail = () => {
         ":rocket: ~ file: index.js ~ line 92 ~ responseGoogle ~ error",
         error
       );
+      setLoading(false);
     }
   };
   return (
-    <div className="single-post">
-      <section class="blog-content p-80">
-        <div class="container">
-          <div class="row">
-            <div class="title-head text-center">
-              <h3>
-                <a href="">{cate}</a>
-              </h3>
-              <h2>
-                <Interweave content={data?.title?.rendered} />
-              </h2>
-              <h5 class="b_date">{dayjs(data?.date).format("MMM D, YYYY")}</h5>
-            </div>
-          </div>
-          <div class="row">
-            <div class="border-line"></div>
-          </div>
-          <div class="row">
-            <div class="col-md-10 col-lg-10 col-sm-10 col-xs-12 offset-md-1 offset-lg-1 offset-sm-1">
-              <div class="right-blog-cnt">
-                <Interweave content={data?.content?.rendered} />
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="border-line"></div>
+    <>
+      {loading ? (
+        <div className="blog-loader">
+          <div class="span">
+            <div class="help"></div>
           </div>
         </div>
-      </section>
-    </div>
+      ) : (
+        <div className="single-post">
+          <section class="blog-content p-80">
+            <div class="container">
+              <div class="row">
+                <div class="title-head text-center">
+                  <h3>
+                    <a href="">{cate}</a>
+                  </h3>
+                  <h2>
+                    <Interweave content={data?.title?.rendered} />
+                  </h2>
+                  <h5 class="b_date">
+                    {dayjs(data?.date).format("MMM D, YYYY")}
+                  </h5>
+                </div>
+              </div>
+              <div class="row">
+                <div class="border-line"></div>
+              </div>
+              <div class="row">
+                <div class="col-md-10 col-lg-10 col-sm-10 col-xs-12 offset-md-1 offset-lg-1 offset-sm-1">
+                  <div class="right-blog-cnt">
+                    <Interweave content={data?.content?.rendered} />
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="border-line"></div>
+              </div>
+            </div>
+          </section>
+        </div>
+      )}
+    </>
   );
 };
 
