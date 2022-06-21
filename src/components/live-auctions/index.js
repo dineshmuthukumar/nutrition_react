d8ed4b2c8fd86a5710c48f2743a9853c5b0ee15e
Abishek_Raja <abishek.r@tranxitgroup.com>
2022-06-21 20:16:56 +0530

    highlight the nft when hover(LP-1226)

--- src/components/live-auctions/index.js
+++ src/components/live-auctions/index.js
@@ -65,7 +65,7 @@ const LiveAuctions = () => {
                 <div className="row">
                   {list.length > 0 ? (
                     <OwlCarousel
-                      className="owl-theme"
+                      className="owl-theme hover-anim px-0"
                       margin={20}
                       nav
                       smartSpeed={500}
