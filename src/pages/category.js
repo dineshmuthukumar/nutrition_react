38e61c6f4db7748e59b5a9bf91411b8f2314cd62
dineshmuthukumar <42163115+dineshmuthukumar@users.noreply.github.com>
2022-10-30 13:16:58 +0530

    Category changes

--- src/pages/category.js
+++ src/pages/category.js
@@ -1,15 +1,7 @@
 import React, { useState, useEffect } from "react";
-import ReactPixel from "react-facebook-pixel";
 ///import { useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-//import Banner from "../components/banner";
-// import LiveAuctions from "../components/live-auctions";
-// import Trending from "../components/trending";
-// //import HotCollections from "../components/hot-collections";
-//import TopBuyers from "../components/top-buyers";
-//import TopSellers from "../components/top-sellers";
-// import RecentlySoldNFT from "../components/recently-sold-nft";
 // import ShowAll from "../components/show-all";
 import { useHistory, useRouteMatch } from "react-router";
 
@@ -17,21 +9,10 @@ import { setCookiesByName, setCookies } from "../utils/cookies";
 import { user_load_by_token_thunk } from "../redux/thunk/user_thunk";
 import { nftCategoriesApi } from "../api/methods";
 import useQuery from "../hook/useQuery";
-//import FavouriteNFTs from "../components/favourite-NFTs";
-// import HeroBanner from "../components/hero-banner";
-// //import NewDropSection from "../components/new-drop-section";
-// // import CollectionList from "../components/collection-list";
-// import TrailerVideo from "../components/trailer-video";
-// import MclGameLaunchTimer from "../components/mcl-game-launch-timer";
-// import MclGameLaunchOne from "../components/mcl-game-launch-one";
-// import MclGameLaunchTwo from "../components/mcl-game-launch-two";
-// import LeaderBoard from "../components/leader-board";
-// import MclCompletedTournaments from "../components/mcl-completed-tournaments";
-// import MclTournaments from "../components/mcl-tournaments";
-// import MclGameButton from "../components/mcl-game-button";
 
 import Header from "../components/header";
 import Product_Category from "../components/new/product_category";
+import { getsubCategoryApi } from "../api/base-methods";
 
 import Footer from "../components/footer";
 
@@ -42,24 +23,9 @@ const Category = () => {
   let query = useQuery();
   const fsz = query.get("fsz");
   const token = query.get("token");
-  const _ga = query.get("_ga");
-  // const { user } = useSelector((state) => state.user.data);
-
-  const [list, setList] = useState([]);
-
-  // const [favPage, setFavPage] = useState(1);
-  // const [favList, setFavList] = useState([]);
-  // const [favLoading, setFavLoading] = useState(false);
-  // const [favHasNext, setFavHasNext] = useState(false);
-
-  const categoriesList = async (page) => {
-    try {
-      let response = await nftCategoriesApi({ page });
-      setList([...list, ...response.data.data.categories]);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const match = useRouteMatch();
+  const { categoryid } = match.params;
+  const [categoryDetails, setCategoryDetails] = useState({});
 
   useEffect(() => {
     if (fsz) {
@@ -73,40 +39,20 @@ const Category = () => {
       history.replace(url);
       dispatch(user_load_by_token_thunk(token));
     }
-
-    ///categoriesList(1);
-    if (_ga) {
-      history.replace(url);
-    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // useEffect(() => {
-  //   if (user) {
-  //     getUserFavedNFTOrders();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [user]);
-
-  // const getUserFavedNFTOrders = async () => {
-  //   try {
-  //     setFavLoading(true);
-  //     const result = await userFavedNFTOrders(favPage);
-  //     setFavList(result.data.data.orders);
-  //     setFavLoading(false);
-  //   } catch (error) {
-  //     console.log(error);
-  //     setFavLoading(false);
-  //   }
-  // };
-
-  // Meta Pixel Code
   useEffect(() => {
-    if (process.env.REACT_APP_MARKETING_SCRIPT === "enabled") {
-      ReactPixel.init(process.env.REACT_APP_META_PIXEL_ID);
-      ReactPixel.pageView();
-    }
+    // if (query.get("forceUpdate") === "true" || !user.name) {
+    getsubCategory();
   }, []);
+
+  const getsubCategory = async () => {
+    const result = await getsubCategoryApi(categoryid);
+    //console.log(result?.data?.responseData?.subCategories);
+    setCategoryDetails(result?.data?.responseData?.subCategories);
+    //console.log(categoryDetails?.bannerImage);
+  };
 
   return (
     <>
@@ -118,19 +64,23 @@ const Category = () => {
       <main className="main single-product">
         <div className="page-content">
           <div className="container-fluid p-0">
-            <div className="page-header pl-4 pr-4" style={{background: "#7ea4b1"}}>
-                <h1 className="page-title font-weight-bold lh-1 text-white text-capitalize">Product Category</h1>
+            <div
+              className="page-header pl-4 pr-4"
+              style={{
+                backgroundImage: `url(${categoryDetails?.bannerImage})`,
+              }}
+            >
+              <h1 className="page-title font-weight-bold lh-1 text-white text-capitalize">
+                {categoryDetails?.subCategoryName}
+              </h1>
             </div>
             <div class="page-content mt-10 pt-10">
-
-                <Product_Category />
-
+              <Product_Category categoryDetails={categoryDetails} />
             </div>
           </div>
         </div>
       </main>
       <Footer />
-      {/* <Footer /> */}
     </>
   );
 };
