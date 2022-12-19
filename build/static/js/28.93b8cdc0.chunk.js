7e07c1e5cdf5bacf3dde82d58b47cb095612e6c0
Nagarajansvs <nagarajansvs@gmail.com>
2022-12-19 15:45:28 +0530

    New Build

--- build/static/js/28.93b8cdc0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpnew-app"]=this["webpackJsonpnew-app"]||[]).push([[28],{701:function(n,p,t){"use strict";t.r(p);t(1);var e=t(4);p.default=function(){return Object(e.jsx)("div",{})}}}]);
-//# sourceMappingURL=28.93b8cdc0.chunk.js.map
