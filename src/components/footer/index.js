261eb7684dee2ed673e345dd1a28001e4b36c5f7
dineshmuthukumar <42163115+dineshmuthukumar@users.noreply.github.com>
2023-02-22 23:30:27 +0530

    whatsup aded

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -488,17 +488,22 @@ const Footer = () => {
 
           <div className="whatsapp-block2">
             <div className="tooltip2 facebook">
-                <span className="tooltipText2 fb">Hey, how can we assist you today?</span>
+              <span className="tooltipText2 fb">
+                Hey, how can we assist you today?
+              </span>
+              <a
+                href={settingsDetails?.site?.whatsAppLink}
+                target="_blank"
+                rel="noopener noreferrer">
+                {" "}
                 <img src="https://img.icons8.com/color/256/whatsapp--v1.png" />
+              </a>
             </div>
           </div>
-
         </div>
       </div>
     </>
   );
 };
-
-
 
 export default Footer;
